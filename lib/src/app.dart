56404ed0ddaf54f79ccba9822d96e09e7c76ec1e
Jerin <jerinji2016@gmail.com>
2024-11-20 23:33:37 +0400

    tidy project

---
+++ lib/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FdawgApp extends StatelessWidget {
+  const FdawgApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
