c27234d9dafac9e47136b02eddb08cc1a8375cb3
Jerin <jerinji2016@gmail.com>
2024-11-30 20:41:24 +0400

    theme improvements

--- lib/src/config/theme_config.dart
+++ lib/src/config/theme_config.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
 
-final lightTheme = ThemeData.light(
-  useMaterial3: true,
-).copyWith(
-  textTheme: ThemeData.light(useMaterial3: true).textTheme.apply(
-        fontFamily: 'Ubuntu Sans',
+const seedColor = Colors.purple;
+const primaryBlack = Color(0xFF0D0D0C);
+
+const hoverDuration = Duration(milliseconds: 200);
+
+ThemeData get lightTheme => ThemeData.light(
+      useMaterial3: true,
+    ).copyWith(
+      splashFactory: InkSparkle.splashFactory,
+      scaffoldBackgroundColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      dividerColor: Colors.grey,
+      cardColor: const Color(0x73FFFFFF),
+      hoverColor: Colors.white54,
+      textTheme: ThemeData.light(useMaterial3: true).textTheme.apply(
+            fontFamily: 'Ubuntu Sans',
+            displayColor: primaryBlack,
+          ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(
+            color: Colors.grey,
+            width: 2,
+          )
+        ),
       ),
-  cardColor: Colors.black12,
-  colorScheme: const ColorScheme.light(),
-);
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: seedColor,
+      ),
+    );
 
 final darkTheme = ThemeData.dark(
   useMaterial3: true,
 ).copyWith(
+  splashFactory: InkSparkle.splashFactory,
+  scaffoldBackgroundColor: Colors.transparent,
   textTheme: ThemeData.dark(useMaterial3: true).textTheme.apply(
         fontFamily: 'Ubuntu Sans',
       ),
-  cardColor: Colors.white10,
-  colorScheme: const ColorScheme.dark(),
+  inputDecorationTheme: InputDecorationTheme(
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+    ),
+  ),
+  cardColor: Colors.black26,
+  colorScheme: const ColorScheme.dark(
+    primary: Colors.purple,
+  ),
 );
