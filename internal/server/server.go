a087dd95561346dcec893e3f69ed7b5d5a58f5d2
Jerin <jerinji2016@gmail.com>
2025-05-14 00:17:05 +0400

    fix: web page serving issue

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-    "fmt"
+    "html/template"
     "net/http"
     "path/filepath"
 
@@ -18,9 +18,32 @@ func Start(port string) error {
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
     utils.Info("Server starting on port %s...", port)
+    utils.Info("Access the web interface at http://localhost:%s", port)
     return http.ListenAndServe(":"+port, nil)
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Flutter Manager Server is running!")
+    // If path is not root, return 404
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+    
+    // Get the template path
+    templatePath := filepath.Join(utils.ProjectRoot(), "web/templates/index.html")
+    
+    // Parse and execute the template
+    tmpl, err := template.ParseFiles(templatePath)
+    if err != nil {
+        utils.Error("Failed to parse template: %v", err)
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+    
+    err = tmpl.Execute(w, nil)
+    if err != nil {
+        utils.Error("Failed to execute template: %v", err)
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
 }
